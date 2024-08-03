064206a3b209dcf80fae244cfa085ef36bb65fe2
edwardanderson <anderson.edw@gmail.com>
2024-08-03 10:23:03 +0200

    add broader relations in code

--- emoji_skos/__init__.py
+++ emoji_skos/__init__.py
@@ -1,6 +1,6 @@
 from emoji_skos.data import ALEXMICK, CARPEDM20
-from pathlib import Path
-from rdflib import Graph
+from rdflib import Graph, URIRef
+from rdflib.namespace import SKOS
 
 
 EMOJI_SKOS = Graph()
@@ -8,9 +8,7 @@ EMOJI_SKOS = Graph()
 for graph in [ALEXMICK, CARPEDM20]:
     EMOJI_SKOS += graph
 
-broader_query_file = Path(__file__).parent / 'broader.rq'
-with open(broader_query_file, 'r') as file:
-    construct = file.read()
-
-result = EMOJI_SKOS.query(construct)
-EMOJI_SKOS += result
+# :human skos:broader :person
+humans = ['👨', '👩', '👩', '👶']
+for item in humans:
+    EMOJI_SKOS.add((URIRef(item), SKOS.broader, URIRef('🧑')))
